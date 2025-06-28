9ed9f3566c5743153993fc83c673eb2898944400
Alex <pluskett.a@gmail.com>
2025-06-28 15:11:15 -0600

    Added asteroid.py, asteroidfield.py. Updated main.py
    -added asteroid.py and asteroidfield.py both with functionality.
    
    -added  asteroid and asteroidfield class container and created an asteroid_field object so that we now have asteroids spawning for the game.
    
    -Change main.py refactored instantiations to group items better for adding or subtracting or adjusting in future.

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     frame_rate = pygame.time.Clock()
     delta_time = 0
-    player_object = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    # Initialize sprite groups
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    player_object.containers = (updateable, drawable)
-        
+    asteroids = pygame.sprite.Group()
+    
+    # Set up class containers
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+    
+    # Create game objects
+    player_object = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+    
+    
     print("Starting Asteroids!")
     print(
 f'''Screen width: {SCREEN_WIDTH} 
